97854f744cb94b007adc92f1d7cebc8b85d6b361
Andrej Kudriavcev <andrej@kudriavcev.info>
2025-09-24 22:44:24 +1000

    Refactor CI configuration to remove test backend jobs and set default values for Azure credentials in variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,17 +18,21 @@ variable "kubernetes_version" {
 variable "subscription_id" {
     description = "Azure Subscription ID"
     type        = string
+    default     = ""
 }
 variable "tenant_id" {
     description = "Azure Tenant ID"
     type        = string
+    default     = ""
 }
 variable "client_id" {
     description = "Azure Client ID"
     type        = string
+    default     = ""
 }
 variable "client_secret" {
     description = "Azure Client Secret"
     type        = string
+    default     = ""
     sensitive   = true
 }
