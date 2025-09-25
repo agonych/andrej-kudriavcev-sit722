483c939e4906b0717160c517516607f77d85e443
Andrej Kudriavcev <andrej@kudriavcev.info>
2025-09-25 22:50:50 +1000

    Refactor CI workflow; remove direct output of storage account key and retrieve it securely from Azure

--- terraform/staging/outputs.tf
+++ terraform/staging/outputs.tf
@@ -8,12 +8,6 @@ output "storage_account_name" {
   value       = azurerm_storage_account.storage.name
 }
 
-output "storage_account_key" {
-  description = "Staging Storage Account Key"
-  value       = azurerm_storage_account.storage.primary_access_key
-  sensitive   = true
-}
-
 output "aks_name" {
   description = "Staging AKS name"
   value       = azurerm_kubernetes_cluster.aks.name
